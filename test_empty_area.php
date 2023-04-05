806290f6c8843d79dfdfd9b9e9c9b4a15511d2e2
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2023-04-05 17:16:09 +0200

    Add files via upload

--- test_empty_area.php
+++ test_empty_area.php
@@ -1,13 +1,17 @@
 <?php
 
-$ret1 = create_user("", "password!");
-$ret2 = create_user("username", "");
-$ret3 = create_user("", "");
-$ret4 = create_user("username", "Password!");
+    $ret1 = create_user("", "password!");
+    $ret2 = create_user("username", "");
+    $ret3 = create_user("", "");
+    $ret4 = create_user("username", "Password!");
 
-if(($ret1 != 0) && ($ret2 != 0) && ($ret3 != 0) && ($ret4 != 1){
+    if(($ret1 == 1) && ($ret2 == 1) && ($ret3 == 1) && ($ret4 == 0)){
 
-}else{
-    printf ("false");
-
+    }else{
+        printf("false");
+        printf($ret1);
+        printf($ret2);
+        printf($ret3);
+        printf($ret4);
+    }
 ?>
