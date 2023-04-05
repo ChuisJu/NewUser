e44b7d745613943294e27bb22f4618d9a9990b30
GitRyderHub <129951841+GitRyderHub@users.noreply.github.com>
2023-04-05 17:11:24 +0200

    Add files via upload

--- test_empty_area.php
+++ test_empty_area.php
@@ -1,5 +1,13 @@
 <?php
 
+$ret1 = create_user("", "password!");
+$ret2 = create_user("username", "");
+$ret3 = create_user("", "");
+$ret4 = create_user("username", "Password!");
 
+if(($ret1 != 0) && ($ret2 != 0) && ($ret3 != 0) && ($ret4 != 1){
+
+}else{
+    printf ("false");
 
 ?>
