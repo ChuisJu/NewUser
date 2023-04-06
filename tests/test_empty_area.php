97c67f0f5c7b1a4d21120fa14c33a090ec1df58e
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2023-04-06 15:46:47 +0200

    Add files via upload

--- tests/test_empty_area.php
+++ tests/test_empty_area.php
@@ -1,12 +1,9 @@
 <?php
-
     include("../new.php");
-
     $ret1 = create_user("", "password!");
     $ret2 = create_user("username", "");
     $ret3 = create_user("", "");
     $ret4 = create_user("username", "Password!");
-
     if(($ret1 == 1) && ($ret2 == 1) && ($ret3 == 1) && ($ret4 == 0)){
         printf("true");
     }else{
--- tests/test_inject.php
+++ tests/test_inject.php
@@ -1,15 +1,8 @@
 <?php
-
     include("../new.php");
-
-
     $ret8 = create_user("' OR '1'='1", "password##");
-
     $ret11 = create_user(";ls -la;", "password##");
-
     $ret14 = create_user("AZLdjAHfoaj", "MotDePasse@99");
-
-
     if(($ret8 == 2) && ($ret11 == 2) && ($ret14 == 0)){
         printf("true"); 
     }else{
@@ -18,7 +11,4 @@
         printf($ret11);
         printf($ret14);
     }
-
-
-
 ?>
