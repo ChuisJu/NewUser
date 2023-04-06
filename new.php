41d56ed3889e71b4d55a334376337a0167d10079
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2023-04-06 15:25:09 +0200

    Add files via upload

--- new.php
+++ new.php
@@ -1,8 +1,8 @@
 <?php
         function create_user($login, $password) {
-            if (!empty($login)) &&  (!empty($password)){
+            if ((!empty($login)) && (!empty($password))){
                 if (!preg_match('/[^A-Za-z0-9]/', $login)) {
-                    if (strlen($password) >= 8) && (strlen($password) <= 64){
+                    if ((strlen($password) >= 8) && (strlen($password) <= 64)){
                         if (strlen($login) <= 25){
                             if (preg_match('/[^A-Za-z0-9]/', $password)) {
                                 if (file_exists($login)){
